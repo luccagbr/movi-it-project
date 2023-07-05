505650f99532e3e54abe0c1d446a2d36d8f77708
Lucca Gabriel <luccavitalc@gmail.com>
2023-07-05 16:57:41 -0300

    x

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -31,6 +31,7 @@ export const Countdown = () => {
                     </div>
                     <span>:</span>
                     <div>
+                        
                         <span>{secondLeft}</span>
                         <span>{secondRight}</span>
                     </div>
