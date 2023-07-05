0b2a0d077020187d0a37337f622e4a8c6cefaeb6
Lucca Gabriel <luccavitalc@gmail.com>
2023-07-05 17:54:39 -0300

    19:38

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -66,6 +66,7 @@ export const Countdown = () => {
                             >
                                 Abandonar Ciclo
                         </button>
+                        
                         ) : (
                         <button 
                             type="button" 
