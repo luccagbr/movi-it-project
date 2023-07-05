85154db3a2325a038699e51220183aa89bc03d2a
Lucca Gabriel <luccavitalc@gmail.com>
2023-07-05 16:46:34 -0300

    Finalização do do painel de tempo e botão ao iniciar o ciclo

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
+export const ExperienceBar = () => {
+    return (
+        <header className={styles.experienceBar}>
+            <span>0 xp</span>
+            <div>
+                <div style = {{ width: "50%"}} />
+
+                <span className={styles.currentExperience} style = {{ left: "50%"}}>300xp</span>
+            </div>
+            <span></span>
+        </header>
+    )
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
